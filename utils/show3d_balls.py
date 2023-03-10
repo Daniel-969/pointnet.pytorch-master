86f8bd8d4daf8ab0a3bc20682b08f89ff1610e50
Daniel-969 <1351461715@qq.com>
2023-03-10 10:54:06 +0800

    添加注释与数据集

--- utils/show3d_balls.py
+++ utils/show3d_balls.py
@@ -2,10 +2,12 @@ import numpy as np
 import ctypes as ct
 import cv2
 import sys
+
 showsz = 800
 mousex, mousey = 0.5, 0.5
 zoom = 1.0
 changed = True
+
 
 def onmouse(*args):
     global mousex, mousey, changed
@@ -15,6 +17,7 @@ def onmouse(*args):
     mousey = y / float(showsz)
     changed = True
 
+
 cv2.namedWindow('show3d')
 cv2.moveWindow('show3d', 0, 0)
 cv2.setMouseCallback('show3d', onmouse)
@@ -22,40 +25,40 @@ cv2.setMouseCallback('show3d', onmouse)
 # dll = np.ctypeslib.load_library('render_balls_so', '.')
 dll = ct.cdll.LoadLibrary('render_balls.dll')
 
-def showpoints(xyz,c_gt=None, c_pred = None, waittime=0, 
-    showrot=False, magnifyBlue=0, freezerot=False, background=(0,0,0), 
-    normalizecolor=True, ballradius=10):
+
+def showpoints(xyz, c_gt=None, c_pred=None, waittime=0,
+               showrot=False, magnifyBlue=0, freezerot=False, background=(0, 0, 0),
+               normalizecolor=True, ballradius=10):
     global showsz, mousex, mousey, zoom, changed
-    xyz=xyz-xyz.mean(axis=0)
-    radius=((xyz**2).sum(axis=-1)**0.5).max()
-    xyz/=(radius*2.2)/showsz
+    xyz = xyz - xyz.mean(axis=0)
+    radius = ((xyz ** 2).sum(axis=-1) ** 0.5).max()
+    xyz /= (radius * 2.2) / showsz
     if c_gt is None:
-        c0 = np.zeros((len(xyz), ), dtype='float32') + 255
-        c1 = np.zeros((len(xyz), ), dtype='float32') + 255
-        c2 = np.zeros((len(xyz), ), dtype='float32') + 255
+        c0 = np.zeros((len(xyz),), dtype='float32') + 255
+        c1 = np.zeros((len(xyz),), dtype='float32') + 255
+        c2 = np.zeros((len(xyz),), dtype='float32') + 255
     else:
         c0 = c_gt[:, 0]
         c1 = c_gt[:, 1]
         c2 = c_gt[:, 2]
-
 
     if normalizecolor:
         c0 /= (c0.max() + 1e-14) / 255.0
         c1 /= (c1.max() + 1e-14) / 255.0
         c2 /= (c2.max() + 1e-14) / 255.0
 
-
     c0 = np.require(c0, 'float32', 'C')
     c1 = np.require(c1, 'float32', 'C')
     c2 = np.require(c2, 'float32', 'C')
 
     show = np.zeros((showsz, showsz, 3), dtype='uint8')
+
     def render():
-        rotmat=np.eye(3)
+        rotmat = np.eye(3)
         if not freezerot:
-            xangle=(mousey-0.5)*np.pi*1.2
+            xangle = (mousey - 0.5) * np.pi * 1.2
         else:
-            xangle=0
+            xangle = 0
         rotmat = rotmat.dot(
             np.array([
                 [1.0, 0.0, 0.0],
@@ -102,6 +105,7 @@ def showpoints(xyz,c_gt=None, c_pred = None, waittime=0,
                         (30, showsz - 50), 0, 0.5, cv2.cv.CV_RGB(255, 0, 0))
             cv2.putText(show, 'zoom %d%%' % (int(zoom * 100)), (30, showsz - 70), 0,
                         0.5, cv2.cv.CV_RGB(255, 0, 0))
+
     changed = True
     while True:
         if changed:
@@ -120,18 +124,18 @@ def showpoints(xyz,c_gt=None, c_pred = None, waittime=0,
         if cmd == ord('t') or cmd == ord('p'):
             if cmd == ord('t'):
                 if c_gt is None:
-                    c0 = np.zeros((len(xyz), ), dtype='float32') + 255
-                    c1 = np.zeros((len(xyz), ), dtype='float32') + 255
-                    c2 = np.zeros((len(xyz), ), dtype='float32') + 255
+                    c0 = np.zeros((len(xyz),), dtype='float32') + 255
+                    c1 = np.zeros((len(xyz),), dtype='float32') + 255
+                    c2 = np.zeros((len(xyz),), dtype='float32') + 255
                 else:
                     c0 = c_gt[:, 0]
                     c1 = c_gt[:, 1]
                     c2 = c_gt[:, 2]
             else:
                 if c_pred is None:
-                    c0 = np.zeros((len(xyz), ), dtype='float32') + 255
-                    c1 = np.zeros((len(xyz), ), dtype='float32') + 255
-                    c2 = np.zeros((len(xyz), ), dtype='float32') + 255
+                    c0 = np.zeros((len(xyz),), dtype='float32') + 255
+                    c1 = np.zeros((len(xyz),), dtype='float32') + 255
+                    c2 = np.zeros((len(xyz),), dtype='float32') + 255
                 else:
                     c0 = c_pred[:, 0]
                     c1 = c_pred[:, 1]
@@ -145,20 +149,21 @@ def showpoints(xyz,c_gt=None, c_pred = None, waittime=0,
             c2 = np.require(c2, 'float32', 'C')
             changed = True
 
-        if cmd==ord('n'):
-            zoom*=1.1
-            changed=True
-        elif cmd==ord('m'):
-            zoom/=1.1
-            changed=True
-        elif cmd==ord('r'):
-            zoom=1.0
-            changed=True
-        elif cmd==ord('s'):
-            cv2.imwrite('show3d.png',show)
-        if waittime!=0:
+        if cmd == ord('n'):
+            zoom *= 1.1
+            changed = True
+        elif cmd == ord('m'):
+            zoom /= 1.1
+            changed = True
+        elif cmd == ord('r'):
+            zoom = 1.0
+            changed = True
+        elif cmd == ord('s'):
+            cv2.imwrite('show3d.png', show)
+        if waittime != 0:
             break
     return cmd
+
 
 if __name__ == '__main__':
     np.random.seed(100)
--- utils/train_classification.py
+++ utils/train_classification.py
@@ -1,40 +1,62 @@
+# 加上这句之后 即使在python2.X，使用print就得像python3.X那样加括号使用。python2.X中print不需要括号，而在python3.X中则需要。
 from __future__ import print_function
-import argparse
+import argparse  # 实现命令行中输入参数的传递
 import os
 import random
 import torch
 import torch.nn.parallel
+# 优化器模块
 import torch.optim as optim
+# 处理数据集的模块
 import torch.utils.data
+# 从pointnet.pytorch/pointnet/dataset.py和pointnet.pytorch/pointnet/model.py中导入库
+# 数据进行预处理的库
 from pointnet.dataset import ShapeNetDataset, ModelNetDataset
+# pointnet的模型结构库
 from pointnet.model import PointNetCls, feature_transform_regularizer
+# 封装好的类
 import torch.nn.functional as F
+# 展示进度条的模块
 from tqdm import tqdm
 
-
+# 使用argparse 的第一步是创建一个 ArgumentParser 对象
 parser = argparse.ArgumentParser()
+# 添加程序参数信息
+# 终端键入batchsize大小
 parser.add_argument(
     '--batchSize', type=int, default=32, help='input batch size')
+# 默认的数据集每个点云是2500个点
 parser.add_argument(
     '--num_points', type=int, default=2500, help='input batch size')
+# 加载数据的进程数目
 parser.add_argument(
     '--workers', type=int, help='number of data loading workers', default=4)
+# epoch，训练多少轮 默认250
 parser.add_argument(
     '--nepoch', type=int, default=250, help='number of epochs to train for')
+# 输出文件夹名称
 parser.add_argument('--outf', type=str, default='cls', help='output folder')
+# 预训练模型路径
 parser.add_argument('--model', type=str, default='', help='model path')
-parser.add_argument('--dataset', type=str, required=True, help="dataset path")
+# 这里，数据集的路径必须手动设置
+parser.add_argument('--dataset', type=str, required=True, help="dataset path", default='.\modelnet40_normal_resampled')
+# 数据集类型
 parser.add_argument('--dataset_type', type=str, default='shapenet', help="dataset type shapenet|modelnet40")
+# 是否进行特征变换
 parser.add_argument('--feature_transform', action='store_true', help="use feature transform")
 
+# 解析参数
 opt = parser.parse_args()
 print(opt)
 
 blue = lambda x: '\033[94m' + x + '\033[0m'
 
+# 返回1～10000间的一个整数，作为随机种子 opt的类型为：<class 'argparse.Namespace'>
 opt.manualSeed = random.randint(1, 10000)  # fix seed
 print("Random Seed: ", opt.manualSeed)
+# 保证在有种子的情况下生成的随机数都是一样的
 random.seed(opt.manualSeed)
+# 设置一个用于生成随机数的种子，返回的是一个torch.Generator对象
 torch.manual_seed(opt.manualSeed)
 
 if opt.dataset_type == 'shapenet':
